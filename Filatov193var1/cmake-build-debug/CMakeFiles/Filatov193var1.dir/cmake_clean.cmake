5a13314af21b4249cde64cdffa15cbd329873af0
gheoblink <72501874+gheoblink@users.noreply.github.com>
2020-12-15 23:37:56 +0300

    Add files via upload

---
+++ Filatov193var1/cmake-build-debug/CMakeFiles/Filatov193var1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Filatov193var1.dir/main.cpp.obj"
+  "Filatov193var1.exe"
+  "Filatov193var1.exe.manifest"
+  "Filatov193var1.pdb"
+  "libFilatov193var1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Filatov193var1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
